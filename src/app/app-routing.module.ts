5af3eea14c743da1404c5c089941897c27cebae9
Edison Giraldo <edisongiraldo@gmail.com>
2021-05-02 16:50:25 -0500

    Creacion de nuevo proyecto ejemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SegundapaginaComponent} from '../app/segundapagina/segundapagina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'segundapagina', component: SegundapaginaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
